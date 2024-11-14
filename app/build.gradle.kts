69ebb88858d4df7a7dbe02dedd50b913b60f639b
LeapVPN <info@leapvpn.org>
2024-11-14 10:54:54 +0800

    add libv2ray

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
 }
 
 dependencies {
-
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar", "*.jar"))))
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
